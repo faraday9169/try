b1dc684a4662fd82a758cdafa9b72a83e58cdc68
unknown <18705656016@163.com>
2022-09-22 18:49:16 +0800

    “fangz”

---
+++ src/com/atguigu/spring5/testdemo/TestSpring5.java
@@ -0,0 +1,18 @@
+package com.atguigu.spring5.testdemo;
+
+import com.atguigu.spring5.Book;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring5 {
+
+    @Test
+    public void testBook1() {
+        ApplicationContext context = new ClassPathXmlApplicationContext( "bean1.xml");
+
+        Book book = context.getBean( "book", Book.class);
+        System.out.println(book);
+        book.testDemo();
+    }
+}
